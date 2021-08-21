7cb7079586c0cd5c32c49c7458b4e7d5b0cbc9ab
m1172 <muhshuh2000@gmail.com>
2021-08-21 10:31:58 +0200

    new

---
+++ filter/src/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {data} from './mock'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
